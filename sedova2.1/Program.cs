0f97e8680e09db8f9c22ee93e611074099242a82
user2 <user2@09.ru>
2021-10-13 13:41:56 +0200

    Добавьте файлы проекта.

---
+++ sedova2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sedova2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите координату x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите координату y:");
+            double y = double.Parse(Console.ReadLine());
+            if (y <= -x + 1 && y >= -x && y <= x && y >= x - 1) Console.WriteLine("Точка попадает в область.");
+            else Console.WriteLine("Точка не попадает в область");
+        }
+    }
+}
